b81cadb7b742c9da50190941362318fa8d79d370
Gittiiiii <s2310238021@fhooe.at>
2025-11-10 20:31:47 +0100

    Middleware

--- GreenKnight/app.js
+++ GreenKnight/app.js
@@ -5,3 +5,23 @@ import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+
+// ---------- Middleware ----------
+
+// Logging Middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// JSON Parser Middleware
+app.use(express.json());
+
+// Mocked Authorization Middleware
+app.use("/api/secure", (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ error: "Nicht eingeloggt" });
+  }
+  next();
+});
